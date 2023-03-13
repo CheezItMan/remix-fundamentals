36dca9e3919445419e06d39fca0f483b9f0c7f34
Chris McAnally <mcanallyc@gmail.com>
2023-03-12 17:44:12 -0700

    data loading exercise

--- exercise/02-data-loading/app/routes/posts/index.tsx
+++ exercise/02-data-loading/app/routes/posts/index.tsx
@@ -1,7 +1,34 @@
+import type { LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { prisma } from "~/db.server";
+
+async function getPosts() {
+  return prisma.
+};
+
+export const loader = async () => {
+  
+  return json({ posts: await getPosts() }); 
+};
+
+
 export default function Posts() {
+  const { posts } = useLoaderData<typeof loader>(); 
   return (
     <main>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => {
+          return (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+          );
+        })}
+      </ul>
     </main>
   );
 }
